a2e2ee2d5617102aff5a9655632f95d07d248ec0
forsythrl <tarrantrl@utexas.edu>
2020-08-23 16:44:21 -0500

    add basic html outline and quiz question array

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answers: ["commas", "curly brackers", "quotes", "parenthesis"],
+        correct: "quotes",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: "parenthesis",
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correct: "console.log",
+    }
+]
